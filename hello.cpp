debb4a21a3f452e8d966700e975affd42bae4604
Wacke Bernd <bernd.wacke@fh-campuswien.ac.at>
2020-11-15 18:33:36 +0100

    test

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    msg.begin();
+    int i = 0;
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+        i++;
+    }
+    cout << endl;
+}
